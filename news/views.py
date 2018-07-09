f970e4d7247cdbdf78c078266385f962911a2d04
wanjiiru <liz240@live.com>
2018-07-09 14:23:17 +0300

    add bootstrap and templates

--- news/views.py
+++ news/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse,Http404
+import datetime as dt
 
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request,'welcome.html')
+
+def news_of_day(request):
+    try:
+        
+        date = dt.date.today()
+    except ValueError:
+        raise Http404()
+    return render(request,'all-news/today-news.html', {"date":date,})
+
+# View Function to present news from past days
+def past_days_news(request, past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_today)
+
+    return render(request, 'all-news/past-news.html', {"date": date}) 
+            
--- tribune/settings.py
+++ tribune/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'news.apps.NewsConfig',
+    'news',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
